7cdfcc4074f27f9cc73f6eb77048e2c46b8822b8
Yishay <yishayash@walla.com>
2018-12-09 13:35:50 +0200

    first model #1 try

--- models/mnist_model.py
+++ models/mnist_model.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
 
 def plot_image(i, predictions_array, true_label, img):
     predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
@@ -19,19 +19,22 @@ def plot_image(i, predictions_array, true_label, img):
     else:
         color = 'red'
 
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label], 100 * np.max(predictions_array), class_names[true_label]), color=color)
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label], 100 * np.max(predictions_array),
+                                         class_names[true_label]), color=color)
     plt.show()
+
 
 mnist = tf.keras.datasets.mnist
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    tf.keras.layers.Conv2D(filters=64, kernel_size=8 * 8, activation=tf.nn.relu),
+    tf.keras.layers.Conv2D(filters=128, kernel_size=6 * 6, activation=tf.nn.relu),
+    tf.keras.layers.Conv2D(filters=128, kernel_size=5 * 5, activation=tf.nn.relu),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
@@ -44,5 +47,5 @@ print('Test accuracy:', test_acc)
 
 predictions = model.predict(x_test)
 i = 6666
-plt.figure(figsize=(6,3))
+plt.figure(figsize=(6, 3))
 plot_image(i, predictions, y_test, x_test)
