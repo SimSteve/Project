59e26e4acfcb6cce80b482e34d438d8e637c3b36
Sim Gutfreund <simgutfreund@gmail.com>
2019-06-20 00:13:49 +0300

    ...

--- src/convert_to_weights.py
+++
@@ -1,9 +0,0 @@
-import src.Models_old as m
-import src.encryptions.unencrypted as e
-
-name = "fashion_mnist_FGSM_PERMUTATED"
-
-model = m.Encrypted_Model(encrypt=e.encrypt)
-model.load(name)
-
-model.model.save_weights('saved_weights/{}.h5'.format(name))
